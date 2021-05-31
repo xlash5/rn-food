e9eb45359f7855122013d1036531ac4fd1eeb691
Enes <menesilhan@gmail.com>
2021-05-31 22:32:16 +0300

    first

---
+++ src/screens/ResultsShowScreen.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, FlatList, Image } from 'react-native';
+import yelp from '../api/yelp';
+
+const ResultsShowScreen = ({ navigation }) => {
+    const id = navigation.getParam('id');
+    const [result, setResult] = useState(null);
+
+    const getResult = async (id) => {
+        const response = await yelp.get(`/${id}`);
+        setResult(response.data);
+    };
+
+    useEffect(() => {
+        getResult(id);
+    }, [])
+
+    if (!result) {
+        return null;
+    }
+
+    return (
+        <View>
+            <Text>{result.name}</Text>
+            <FlatList
+                data={result.photos}
+                keyExtractor={(photo) => photo}
+                renderItem={({ item }) => {
+                    return <Image style={styles.image} source={{ uri: item }} />
+                }}
+            />
+        </View>
+    );
+};
+
+export default ResultsShowScreen;
+
+const styles = StyleSheet.create({
+    image: {
+        height: 200,
+        width: 300,
+    }
+});
---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import SearchBar from '../components/SearchBar'
+import yelp from '../api/yelp';
+import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
+
+const SearchScreen = () => {
+    const [term, setTerm] = useState('');
+    const [searchApi, results, errorMessage] = useResults();
+
+    const filterResultsByPrice = (price) => {
+        return results.filter(result => {
+            return result.price === price;
+        });
+    };
+
+    return (
+        <>
+            <SearchBar
+                placeholder="search"
+                term={term}
+                onTermChange={setTerm}
+                onTermSubmit={() => searchApi(term)}
+            />
+            {errorMessage ? <Text>{errorMessage}</Text> : null}
+            <ScrollView>
+                <ResultsList
+                    results={filterResultsByPrice('$')}
+                    title="Cost Effective"
+                />
+                <ResultsList
+                    results={filterResultsByPrice('$$')}
+                    title="Bit Pricier"
+                />
+                <ResultsList
+                    results={filterResultsByPrice('$$$')}
+                    title="Big Spender"
+                />
+            </ScrollView>
+        </>
+    );
+};
+
+export default SearchScreen;
+
+const styles = StyleSheet.create({});
