ccc32efd575a906b0c12030b0baf0f1d163e7243
Muhammet Enes Ilhan <58420282+xlash5@users.noreply.github.com>
2021-05-31 22:39:30 +0300

    Update yelp.js

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
-        Authorization: 'Bearer s4jM2cSFjPbDWOGRarlghRCYzXLUFdKcer7AD9wQ_1Ejl48Dpy5lEi00dultXZUAf7uWRA2j0jZkVf-6x78h_7vJK3Sgll0M2q_UpWMd6yvRbRBnBLAScfpQBlWqYHYx'
+        Authorization: 'Bearer YOUR_YELP_API_KEY'
     }
 });
